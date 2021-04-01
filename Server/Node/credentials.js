b3c28ed0962b43f0ba8ffa75e58537bb981a3bec
RonaldOrdonez <ronald.inge2013@gmail.com>
2021-03-31 22:06:57 -0600

    se agregaron las credenciales nuevas

---
+++ Server/Node/credentials.js
@@ -0,0 +1,15 @@
+let aws_keys = {
+    s3: {
+        //apiVersion: '2006-03-01',
+        region: 'us-east-2',
+        accessKeyId: "AKIASKVPP37UDLPRJ3MS",
+        secretAccessKey: "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"        
+    },
+    dynamodb: {
+        apiVersion: '2012-08-10',
+        region: 'us-east-2',
+        accessKeyId: "AKIASKVPP37UEJNJHRUA",
+        secretAccessKey: "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"
+    }
+}
+module.exports = aws_keys
