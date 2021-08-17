66926e1b2236c87b2ababd307dd14497e96939ee
A <201318652@ingenieria.usac.edu.gt>
2021-08-16 20:28:58 -0600

    Update credentials.js

--- Server/Node/credentials.js
+++ Server/Node/credentials.js
@@ -2,24 +2,24 @@ let aws_keys = {
     s3: {
         //apiVersion: '2006-03-01',
         region: 'us-east-2',
-        accessKeyId: "AKIASKVPP37UDLPRJ3MS",
-        secretAccessKey: "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"        
+        accessKeyId: "",
+        secretAccessKey: ""        
     },
     dynamodb: {
         apiVersion: '2012-08-10',
         region: 'us-east-2',
-        accessKeyId: "AKIASKVPP37UEJNJHRUA",
-        secretAccessKey: "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"
+        accessKeyId: "",
+        secretAccessKey: ""
     },
     rekognition: {        
         region: 'us-east-2',
-        accessKeyId: "AKIASKVPP37UCBXPBGPC",
-        secretAccessKey: "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"        
+        accessKeyId: "",
+        secretAccessKey: ""        
     },
     translate:{
         region: 'us-east-2',
-        accessKeyId: "AKIASKVPP37UCY75VAVJ",
-        secretAccessKey: "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"        
+        accessKeyId: "",
+        secretAccessKey: ""        
     }
 
 }
